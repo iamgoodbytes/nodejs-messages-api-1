7cb8acedd6b0a4eb1c43f46264f5f4a703aec7d8
Joris Hens <joris@goodbytes.be>
2023-10-11 16:29:56 +0200

    feat: install express

---
+++ routes/api/v1/messages.js
@@ -0,0 +1,21 @@
+// require express
+const express = require("express");
+// create a new Router
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        status: "success",
+        message: "GET messages",
+    });
+});
+
+router.post("/", (req, res) => {
+    let message = req.body.message;
+    res.json({
+        status: "success",
+        message: `POST ${message}`,
+    });
+});
+
+module.exports = router;
